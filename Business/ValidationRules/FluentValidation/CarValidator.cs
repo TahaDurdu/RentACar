cc021723e49ca64e8532eef80fb93b93668eb182
tahadurdu <tahadurdu@hotmail.com>
2024-05-01 13:50:36 +0300

    Fluentvalidation desteği eklendi

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+	public class CarValidator:AbstractValidator<Car>
+	{
+		public CarValidator()
+		{
+			RuleFor(p => p.CarName).NotEmpty();
+			RuleFor(p => p.CarName).MinimumLength(2);
+			RuleFor(p => p.DailyPrice).GreaterThan(0);
+			RuleFor(p => p.DailyPrice).NotEmpty();
+			RuleFor(p => p.ModelYear).NotEmpty();
+		}
+	}
+}
+
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentValidation;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+	public class ValidationTool
+	{
+		public static void Validate(IValidator validator,object entitiy)
+		{
+			var context = new ValidationContext<object>(entitiy);
+			var result = validator.Validate(context);
+
+			if (!result.IsValid)
+			{
+
+				throw new ValidationException(result.Errors);
+			}
+		}
+	}
+}
+
